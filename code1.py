99f5cc49092bf59ecfa4fa5374aed94efaddf8a3
k20940rc <reuben.cruise@student.manchester.ac.uk>
2023-11-27 14:40:57 +0000

    working through web.py tutorial

---
+++ code1.py
@@ -0,0 +1,15 @@
+import web
+render = web.template.render('templates/')
+
+urls = (
+    '/', 'index'
+)
+
+class index:
+    def GET(self):
+        name = 'Bob'
+        return render.index(name)
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
