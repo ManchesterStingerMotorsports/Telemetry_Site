c80c053698c01eaceac9a6ea46184bb6fd7724d0
Croose <reubenfc@gmail.com>
2023-12-06 11:44:31 +0000

    Better auto scroll

--- script.js
+++ script.js
@@ -1,56 +1,55 @@
 
-let exp = "Math.sin(x)";
-let exp2 = "Math.cos(x)";
+// tracks x
+var last_x = 0;
+const graph_range_limit = 40;
+const graph_div_id = 'myPlot';
 
 // Generate values
 const xValues = [];
 const yValues = [];
+
 for (let x = 0; x <= 10; x += 0.1) {
     xValues.push(x);
-    yValues.push(eval(exp));
+    yValues.push(Math.sin(last_x));
 }
 
-
-const yValues2 = [];
-for (let x = 0; x <= 10; x += 0.1) {
-    yValues2.push(eval(exp2));
-}
-
-var lastX = 10
+last_x = 10
 
 // Display using Plotly
-const data = [{ x: xValues, y: yValues, mode: "lines" },
-{ x: xValues, y: yValues2, mode: "lines" }];
-const layout = { title: "ECU Data",
-yaxis: {title: "Sensor Readouts"},
-xaxis: {title: "Time"}};
+// const data = [{ x: xValues, y: yValues, mode: "lines" }];
+// const layout = { title: "test" };
+
+//Display using Plotly
+const data = [{ x: [last_x], y: [Math.sin(last_x)], mode: "lines" },
+{ x: [last_x], y: [Math.cos(last_x)], mode: "lines" }];
+const layout = {
+    title: "ECU Data",
+    yaxis: { title: "Sensor Readouts" },
+    xaxis: { title: "Time" }
+};
 
 const configs = {
-    
     responsive: true,
     autosize: true,
-    width: 500,
-    height: 500,
-    automargin: true,
 };
 
 window.onload = function () {
-    Plotly.newPlot("myPlot", data, layout, configs);
-    setInterval(updatePlot, 10)
+    Plotly.newPlot(graph_div_id, data, layout);
+    setInterval(updatePlot, 10);
 }
 
 function updatePlot() {
-    lastX += 0.1;
-    x = lastX;
-    // removes first element of array
-    // xValues.shift();
-    // yValues.shift();
-    // yValues2.shift();
+    last_x += 0.1;
 
-    // appends new values
-    xValues.push(x);
-    yValues.push(eval(exp))
-    yValues2.push(eval(exp2))
+    var new_data = { y: [[Math.sin(last_x)], [Math.cos(last_x)]], x: [[last_x], [last_x]] };
+    Plotly.extendTraces(graph_div_id, new_data, [0, 1])
 
-    Plotly.redraw('myPlot')
+    if (last_x > graph_range_limit) {
+        Plotly.update(graph_div_id, {}, { xaxis: { range: [last_x - graph_range_limit, last_x] } });
+    }
+
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
