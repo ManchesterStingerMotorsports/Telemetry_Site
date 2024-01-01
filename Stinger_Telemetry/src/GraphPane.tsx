6f967c1252d941ac639f79bb015bc007b2135ed4
k20940rc <reuben.cruise@student.manchester.ac.uk>
2024-01-01 18:42:18 +0000

    dev dependencies

--- Stinger_Telemetry/src/GraphPane.tsx
+++ Stinger_Telemetry/src/GraphPane.tsx
@@ -1,10 +1,23 @@
 import './GraphPane.css';
+import Plot from 'react-plotly.js';
 const GraphPane = () => {
   return (
     <div className='container-fluid graphPane'>
       <div className='row'>
         <div className='col-10'>
-          <p>Graph Goes here</p>
+          <Plot
+            data={[
+              {
+                x: [1, 2, 3],
+                y: [2, 6, 3],
+                type: 'scatter',
+                mode: 'lines+markers',
+                marker: { color: 'red' },
+              },
+              { type: 'bar', x: [1, 2, 3], y: [2, 5, 3] },
+            ]}
+            layout={{ width: 320, height: 240, title: 'A Fancy Plot' }}
+          />
         </div>
         <div className='col-2 sidePanel'>
           <p>side panel goes here</p>
