76ad95a93b92f36ea94f23fd0e01642646c2cc42
k20940rc <reuben.cruise@student.manchester.ac.uk>
2024-01-08 10:15:56 +0000

    progress

--- Stinger_Telemetry/src/GraphPane.tsx
+++ Stinger_Telemetry/src/GraphPane.tsx
@@ -1,26 +1,58 @@
+import { useState, useEffect } from 'react';
 import './GraphPane.css';
 import Plot from 'react-plotly.js';
 const GraphPane = () => {
+  const [current_x, setCurrent_x] = useState(6);
+
+  const [data, setData] = useState([
+    { name: 'rpm', x: [1, 2, 3, 4, 5, 6], y: [2, 4, 5, 8, 10, 12] },
+    { name: 'temp', x: [1, 2, 3, 4, 5, 6], y: [21, 34, 35, 38, 20, 32] },
+  ]);
+
+  const updateData = (): void => {
+    setCurrent_x(current_x + 0.1);
+    const new_data = [...data];
+    new_data.forEach((trace) => {
+      trace.x.push(current_x);
+      trace.y.push(random_integer(20));
+    });
+    setData(new_data);
+    console.log(new_data);
+  };
+  function random_integer(maximum_value: number): number {
+    return Math.floor(Math.random() * maximum_value);
+  }
+
+  useEffect(() => {
+    setInterval(updateData, 100);
+  }, []);
+
   return (
     <div className='container-fluid graphPane'>
       <div className='row'>
         <div className='col-10'>
           <Plot
+            divId='plotOne'
             data={[
               {
-                x: [1, 2, 3],
-                y: [2, 6, 3],
+                x: data[0].x,
+                y: data[0].y,
                 type: 'scatter',
-                mode: 'lines+markers',
-                marker: { color: 'red' },
+                name: data[0].name,
               },
-              { type: 'bar', x: [1, 2, 3], y: [2, 5, 3] },
+              {
+                x: data[1].x,
+                y: data[1].y,
+                type: 'scatter',
+                name: data[1].name,
+              },
             ]}
-            layout={{ width: 320, height: 240, title: 'A Fancy Plot' }}
+            layout={{}}
+            config={{ responsive: true }}
           />
         </div>
         <div className='col-2 sidePanel'>
-          <p>side panel goes here</p>
+          <button onClick={updateData}>Click</button>
         </div>
       </div>
     </div>
